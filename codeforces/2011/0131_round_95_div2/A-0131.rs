900df74c6371533809b1eac82a35fa8852901e9e
ykmc <34961813+ykmc@users.noreply.github.com>
2019-11-03 11:13:41 +0900

    Accepted A-0131.rs (round 95 div2)

---
+++ codeforces/2011/0131_round_95_div2/A-0131.rs
@@ -0,0 +1,42 @@
+#![allow(non_snake_case, unused_imports, deprecated, dead_code)]
+
+// Input macros
+macro_rules! input { (source = $s:expr, $($r:tt)*) => { let mut iter = $s.split_whitespace(); let mut next = || { iter.next().unwrap() }; input_inner!{next, $($r)*} }; ($($r:tt)*) => { let stdin = std::io::stdin(); let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock())); let mut next = move || -> String{ bytes.by_ref().map(|r|r.unwrap() as char).skip_while(|c|c.is_whitespace()).take_while(|c|!c.is_whitespace()).collect() }; input_inner!{next, $($r)*} }; }
+macro_rules! input_inner { ($next:expr) => {}; ($next:expr, ) => {}; ($next:expr, $var:ident : $t:tt $($r:tt)*) => { let $var = read_value!($next, $t); input_inner!{$next $($r)*} }; ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => { let mut $var = read_value!($next, $t); input_inner!{$next $($r)*} }; }
+macro_rules! read_value { ($next:expr, ( $($t:tt),* )) => { ( $(read_value!($next, $t)),* ) }; ($next:expr, [ $t:tt ; $len:expr ]) => { (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>() }; ($next:expr, [ $t:tt ]) => { { let len = read_value!($next, usize); (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>() } }; ($next:expr, chars) => { read_value!($next, String).chars().collect::<Vec<char>>() }; ($next:expr, bytes) => { read_value!($next, String).into_bytes() }; ($next:expr, usize1) => { read_value!($next, usize) - 1 }; ($next:expr, $t:ty) => { $next().parse::<$t>().expect("Parse error") }; }
+
+// Module
+use std::cmp::{min,max};
+use std::collections::{VecDeque,HashSet,HashMap,BinaryHeap};
+// Module for v1.15.1
+use std::ascii::AsciiExt; // deprecated since 1.26.0: use inherent method instead
+
+// Functions
+
+// Main
+fn main() {
+
+    input! {
+        mut W: chars,
+    }
+
+    let mut f = true;
+    for i in 1..W.len(){
+        if W[i].is_ascii_lowercase() {
+            f = false;
+            break;
+        }
+    }
+
+    if f {
+        for i in 0..W.len() {
+            if W[i].is_ascii_lowercase() {
+                W[i] = W[i].to_ascii_uppercase();
+            } else {
+                W[i] = W[i].to_ascii_lowercase();
+            }
+        }
+    }
+
+    println!("{}", W.into_iter().collect::<String>());
+}
