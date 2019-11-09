0b23d8ad72905f7362d48d175067d6bad1865a6f
ykmc <34961813+ykmc@users.noreply.github.com>
2019-11-09 12:40:25 +0900

    accepted: D.py (ABC044, ARC060)

--- atcoder/2016/0828_ABC044_ARC060/D.py
+++ atcoder/2016/0828_ABC044_ARC060/D.py
@@ -3,19 +3,36 @@ import sys
 input = sys.stdin.readline
 
 # main
-S = input()
+N = int(input())
+S = int(input())
 
-N = len(S)
-ans = [-1,-1]
+def f(b,n):
+    if n < b:
+        return n
+    else:
+        return f(b, n//b) + n%b
 
-for i in range(N-1):
-    if S[i]==S[i+1]:
-        ans = [i+1,i+2]
-        break
+ans = -1
+if S==N:
+    ans = N+1
+else:
+    # bをN^(1/2)まで全探索
+    for b in range(2, int(N**.5)+1):
+        if f(b,N)==S:
+            ans = b
+            break
 
-for i in range(N-2):
-    if S[i]==S[i+2]:
-        ans = [i+1, i+3]
-        break
+# まだ答えが見つからなければ
+if ans == -1:
+    # bがN^(1/2)より大きい可能性が残っており、b進数で表すと2桁
+    # 1 <= p < b, N = pb+q <= pb < p^2, p > N^(1/2) なので pで全探索
+    # p+q = S より b = (N-S)/p+1
+    # bの最小値を求めるためには大きなpから全探索する必要あり
+    for p in range(int(N**.5)+1,0,-1):
+        b = (N-S)//p + 1
+        # bが1のときは対象外にしないと無限ループ！
+        if b >= 2 and f(b,N)==S:
+            ans = b
+            break
 
-print(ans[0], ans[1])
+print(ans)
