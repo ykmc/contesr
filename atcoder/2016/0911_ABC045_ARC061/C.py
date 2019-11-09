746db1011a7d7a958b3736b9d40f832d7059d9dd
ykmc <34961813+ykmc@users.noreply.github.com>
2019-11-09 13:34:55 +0900

    accepted: C.py (ABC045, ARC061)

---
+++ atcoder/2016/0911_ABC045_ARC061/C.py
@@ -0,0 +1,16 @@
+# python3 (3.4.3)
+import sys
+input = sys.stdin.readline
+
+# main
+from itertools import product
+S = input().rstrip()
+
+ans = 0
+ops = list(product("+ ", repeat=len(S)-1))
+
+for op in ops:
+    s = S[0] + "".join(x+y for (x,y) in zip(op,S[1::])).replace(" ","")
+    ans += eval(s)
+
+print(ans)
