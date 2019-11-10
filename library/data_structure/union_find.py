2fe48b345cf06a745d269fa7fcb43c1e2713997d
ykmc <34961813+ykmc@users.noreply.github.com>
2019-11-10 20:23:48 +0900

    add: union_find.py in /library/data_structure

---
+++ library/data_structure/union_find.py
@@ -0,0 +1,24 @@
+class UnionFind:
+    def __init__(self, n):
+        self.par  = [i for i in range(n)]
+        self.rank = [0] * (n)
+
+    def find(self, x):
+        if self.par[x] == x:
+            return x
+        else:
+            self.par[x] = self.find(self.par[x])
+            return self.par[x]
+ 
+    def unite(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        if self.rank[x] < self.rank[y]:
+            self.par[x] = y
+        else:
+            self.par[y] = x
+            if self.rank[x] == self.rank[y]:
+                self.rank[x] += 1
+
+    def is_same(self, x, y):
+        return self.find(x) == self.find(y)
