d62c14264f24b9bc497e845fee74b8ec69b991fc
ykmc <34961813+ykmc@users.noreply.github.com>
2019-11-12 19:55:58 +0900

    accepted: D.py (ABC051)

---
+++ atcoder/2017/0107_ABC051/D.py
@@ -0,0 +1,23 @@
+# python3 (3.4.3)
+import sys
+input = sys.stdin.readline
+
+# main
+from scipy.sparse.csgraph import csgraph_from_dense, floyd_warshall
+N,M = map(int,input().split())
+ABC = [list(map(int,input().split())) for _ in range(M)]
+
+g = [[0 for _ in range(N)] for _ in range(N)]
+
+for a,b,c in ABC:
+    g[a-1][b-1] = c
+
+csg = csgraph_from_dense(g)
+d = floyd_warshall(csg, directed=False)
+
+ans = 0
+for a,b,c in ABC:
+    if c != d[a-1][b-1]:
+        ans += 1
+
+print(ans)
