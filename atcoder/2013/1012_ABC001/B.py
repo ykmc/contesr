7bd25260b571d73e04fcb7c9be5f85042b1ad3af
ykmc <34961813+ykmc@users.noreply.github.com>
2019-11-13 20:38:25 +0900

    accepted: B.py (ABC001)

---
+++ atcoder/2013/1012_ABC001/B.py
@@ -0,0 +1,20 @@
+# python3 (3.4.3)
+import sys
+input = sys.stdin.readline
+
+# main
+M = int(input())
+
+ans = 0
+if M < 100:
+    ans = 0
+elif M <= 5000:
+    ans = M//100
+elif M <= 30000:
+    ans = M//1000 + 50
+elif M <= 70000:
+    ans = (M//1000-30)//5 + 80
+else:
+    ans = 89
+
+print("{0:02d}".format(ans))
