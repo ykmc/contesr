99acdccd82a629875fdfe82e060b998fba3b910e
ykmc <34961813+ykmc@users.noreply.github.com>
2019-11-17 09:30:41 +0900

    accepted: B.py (ABC036)

---
+++ atcoder/2016/0409_ABC036/B.py
@@ -0,0 +1,13 @@
+# atcoder : python3 (3.4.3)
+import sys
+input = sys.stdin.readline
+
+# main
+N = int(input())
+S = [input().rstrip() for _ in range(N)]
+
+for i in range(N):
+    ans = []
+    for j in range(N):
+        ans.append(S[N-1-j][i])
+    print("".join(ans))
