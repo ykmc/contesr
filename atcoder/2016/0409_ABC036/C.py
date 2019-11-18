c1d3556c02224e4913b579d1b9b2147c7e88802d
ykmc <34961813+ykmc@users.noreply.github.com>
2019-11-18 21:05:45 +0900

    accepted: C.py (ABC036)

---
+++ atcoder/2016/0409_ABC036/C.py
@@ -0,0 +1,15 @@
+# atcoder : python3 (3.4.3)
+import sys
+input = sys.stdin.readline
+
+# main
+N = int(input())
+A = [int(input()) for _ in range(N)]
+
+sa = set(A)
+la = sorted(sa)
+
+ma = {x:i for i, x in enumerate(la)}
+
+for a in A:
+    print(ma[a])
